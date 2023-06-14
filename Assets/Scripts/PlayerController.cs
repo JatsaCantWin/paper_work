92e11fa4c805b2cf04ce75c1179fc086ec69880c
Piotr Jurek <piotrjurek9807@gmail.com>
2023-06-14 22:44:05 +0200

    refined player controls (#21)
    * feat: camera follows player with a delay
    
    * feat: player rotates to match the direction travelled

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.LowLevel;
 
 public class PlayerController : MonoBehaviour
 {
@@ -7,17 +9,22 @@ public class PlayerController : MonoBehaviour
     public float moveHorizontallyDistance = 10f;
     public float keyPressSensitivity = 1.25f;
     public float moveSpeed = 10f;
+    public float cameraMovementDelay = 0.08f;
     public float fadeOutDuration = 0.5f;
     public float fadeInDuration = 0.5f;
 
     private bool _canMove = true;
     private FadeController _fadeController;
     private MovementController _movementController;
-
+    private GameObject _mainCamera;
+    private MovementController _mainCameraMovementController;
+    
     private void Start()
     {
         _fadeController = GetComponent<FadeController>();
         _movementController = GetComponent<MovementController>();
+        _mainCamera = GameObject.FindWithTag("MainCamera");
+        _mainCameraMovementController = _mainCamera.GetComponent<MovementController>();
     }
 
     private void Update()
@@ -43,6 +50,14 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void OrientPlayer(Vector3 direction)
+    {
+        var currentRotation = transform.eulerAngles;
+        var rotationAngle = 90 - (direction.x * 90);
+
+        transform.eulerAngles = new Vector3(currentRotation.x, rotationAngle, currentRotation.z);
+    }
+
     private IEnumerator MoveCoroutine(Vector3 direction, float distance, bool vertical)
     {
         _canMove = false;
@@ -57,14 +72,36 @@ public class PlayerController : MonoBehaviour
         var startPosition = transform.position;
         var targetPosition = startPosition + direction * distance;
 
-        yield return _movementController.MoveCoroutine(moveTime, startPosition, targetPosition);
+        StartCoroutine(_movementController.MoveCoroutine(moveTime, startPosition, targetPosition));
 
+        if (!vertical)
+        {
+            OrientPlayer(direction);
+            yield return new WaitForSeconds(cameraMovementDelay);
+        }
+
+        yield return MoveCameraCoroutine(direction, distance);
+        
         if (vertical)
         {
             _fadeController.FadeIn(fadeOutDuration);
             yield return _fadeController.WaitForFadeCoroutine();
         }
         
+        if (!vertical)
+        {
+            yield return new WaitForSeconds(cameraMovementDelay);
+        }
+        
         _canMove = true;
+    }
+
+    private IEnumerator MoveCameraCoroutine(Vector3 direction, float distance)
+    {
+        var moveTime = distance / moveSpeed;
+        var startPosition = _mainCamera.transform.position;
+        var targetPosition = startPosition + direction * distance;
+
+        yield return _mainCameraMovementController.MoveCoroutine(moveTime, startPosition, targetPosition);
     }
 }
