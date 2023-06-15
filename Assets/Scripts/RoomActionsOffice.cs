f87c26d29b9cf0c688cfe248150ad50711304f17
Piotr Jurek <piotrjurek9807@gmail.com>
2023-06-15 16:17:00 +0200

    feat: basic stamp collection prototype

--- Assets/Scripts/RoomActionsOffice.cs
+++ Assets/Scripts/RoomActionsOffice.cs
@@ -1,5 +1,37 @@
+using System.Collections;
+using UnityEngine;
+
 public class RoomActionsOffice : RoomActionsHorizontalMovement
 {
-    public override void ButtonUp(){}
+    public float stampWaitTime = 0.5f;
+    private bool _playerVisited = false;
+
     public override void ButtonDown(){}
+
+    public override void ButtonUp()
+    {
+        if (!playerController.canMove)
+            return;
+
+        StartCoroutine(AddStamp());
+    }
+    
+    private IEnumerator AddStamp()
+    {
+        playerController.canMove = false;
+
+        if (_playerVisited)
+        {
+            Debug.Log("PlayerAlreadyVisited");
+        }
+        else
+        {
+            Debug.Log("PlayerVisited");
+            _playerVisited = true;
+        }
+
+        yield return new WaitForSeconds(stampWaitTime);
+
+        playerController.canMove = true;
+    } 
 }
