5fd60e18c33badae8170ad2c171227dd38e033f4
Piotr Jurek <piotrjurek9807@gmail.com>
2023-06-15 16:58:28 +0200

    feat: a bubble appears when getting a response from an office worker

--- Assets/Scripts/RoomActionsOffice.cs
+++ Assets/Scripts/RoomActionsOffice.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class RoomActionsOffice : RoomActionsHorizontalMovement
 {
+    public GameObject responseOK;
+    public GameObject responseNO;
     public float stampWaitTime = 0.5f;
     private bool _playerVisited = false;
 
@@ -20,18 +22,39 @@ public class RoomActionsOffice : RoomActionsHorizontalMovement
     {
         playerController.canMove = false;
 
+        GameObject response;
+        
         if (_playerVisited)
         {
-            Debug.Log("PlayerAlreadyVisited");
+            response = responseNO;
         }
         else
         {
-            Debug.Log("PlayerVisited");
+            response = responseOK;
             _playerVisited = true;
         }
+        
+        response.transform.localScale = Vector3.one * 0.01f;
+
+        response.SetActive(true);
+
+        var scaleSpeed = 1.0f / stampWaitTime;
+
+        while (response.transform.localScale.x < 1.0f)
+        {
+            response.transform.localScale += Vector3.one * Time.deltaTime * scaleSpeed;
+            yield return null;
+        }
+
+        response.transform.localScale = Vector3.one;
 
         yield return new WaitForSeconds(stampWaitTime);
 
+        response.transform.localScale = Vector3.one * 0.01f;
+
+        response.SetActive(false);
+        
         playerController.canMove = true;
-    } 
+    }
+
 }
