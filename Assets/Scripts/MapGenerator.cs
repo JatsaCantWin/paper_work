4efe1bfc472b587a31594d08fe19bbd859528677
Piotr Jurek <piotrjurek9807@gmail.com>
2023-06-16 15:21:04 +0200

    feat: random map generation (#37)
    * #12 added random generation of the building
    
    * #12 Improved building generation
    
    * #12 Fixed building generation (corridors with stairs)
    
    * #12 finished building generation
    
    * #12 working fine
    
    * fix: refactored the map generation code to not freeze the game
    
    ---------
    
    Co-authored-by: Kazan33333 <kazan.kk@gmail.com>

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,52 +1,88 @@
+using System;
 using UnityEngine;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
 
 public class MapGenerator : MonoBehaviour
 {
     public GameObject roomCorridor;
     public GameObject roomCorridorDoor;
     public GameObject roomOffice;
+
+    public int numFloors = 5;
+    public int roomsPerFloor = 5;
+    public int entranceCoordX = 0;
+    public int entranceCoordY = 0;
     
     public float spaceBetweenRooms;
     public float spaceBetweenFloors;
     public float scale;
     public float roomWidth;
     public float roomHeight;
-    
-    public int[,] roomArray = {
-        {10, 20, 20, 30, 30},
-        {10, 20, 30, 0, 0}
-    };
 
-    public GameObject[,] gameMap = new GameObject[2,5];
-    
+    public GameObject[,] gameMap;
+
+    private const int RoomTypeNone = 0;
+    private const int RoomTypeEmpty = 10;
+    private const int RoomTypeStairs = 20;
+    private const int RoomTypeOffice = 30;
+    private const int RoomTypeEntrance = 10;
+
+    private const int MaxOfficeRooms = 3;
+
     private void Awake()
     {
-        GenerateRooms();
+        gameMap = new GameObject[numFloors, roomsPerFloor];
+        GenerateMap();
     }
 
-    private void GenerateRooms()
+    private void GenerateMap()
     {
-        roomWidth = roomWidth * scale;
-        roomHeight = roomHeight * scale;
+        int[,] roomArray = new int[numFloors, roomsPerFloor];
 
-        for (int row = 0; row < roomArray.GetLength(0); row++)
+        Debug.Log(roomArray.GetLength(0));
+        
+        GenerateStairs(roomArray);
+        GenerateRemainingRooms(roomArray);
+        GenerateOfficeRooms(roomArray);
+
+        for (int i = 0; i < roomArray.GetLength(0); i++)
         {
-            for (int col = 0; col < roomArray.GetLength(1); col++)
+            String floorString = "Floor " + i + " ";
+            for (var j = 0; j < roomArray.GetLength(1); j++)
+            {
+                floorString += roomArray[i, j] + " ";
+            }
+            Debug.Log(floorString);
+        }
+
+        roomWidth *= scale;
+        roomHeight *= scale;
+
+        for (int row = 0; row < numFloors; row++)
+        {
+            for (int col = 0; col < roomsPerFloor; col++)
             {
                 int roomType = roomArray[row, col];
                 GameObject roomPrefab = null;
 
+                if (row == 0 && col == 0)
+                {
+                    // Bottom right corner room is always a corridor room
+                    roomType = 10;
+                }
+                
                 switch (roomType)
                 {
-                    case 0:
+                    case RoomTypeNone:
                         break;
-                    case 10:
+                    case RoomTypeEmpty:
                         roomPrefab = roomCorridor;
                         break;
-                    case 20:
+                    case RoomTypeStairs:
                         roomPrefab = roomCorridorDoor;
                         break;
-                    case 30:
+                    case RoomTypeOffice:
                         roomPrefab = roomOffice;
                         break;
                     default:
@@ -71,6 +107,81 @@ public class MapGenerator : MonoBehaviour
         ConnectRooms();
     }
 
+    private void GenerateStairs(int[,] roomArray)
+    {
+        var prevRandomCol = -1;
+        var randomCol = -1;
+
+        for (var row = 0; row < numFloors-1; row+=2)
+        {
+            while (randomCol == prevRandomCol)
+                randomCol = Random.Range(0, roomsPerFloor);
+
+            prevRandomCol = randomCol;
+
+            roomArray[row, randomCol] = RoomTypeStairs;
+            roomArray[row+1, randomCol] = RoomTypeStairs;
+            if (row != 0)
+            {
+                roomArray[row - 1, randomCol] = RoomTypeStairs;
+            }
+        }
+
+        // If the map height is uneven, add a third set of stairs to the top floor
+        if (roomArray.GetLength(0) % 2 == 1)
+        {
+            roomArray[roomArray.GetLength(0) - 1, randomCol] = RoomTypeStairs;
+        }
+    }
+
+    void GenerateRemainingRooms(int[,] roomArray)
+    {
+        for (int row = 0; row < numFloors; row++)
+        {
+            for (int col = 0; col < roomsPerFloor; col++)
+            {
+                if (roomArray[row, col] == 0)
+                {
+                    int roomType = 10;
+                    roomArray[row, col] = roomType;
+                }
+            }
+        }
+    }
+
+    void GenerateOfficeRooms(int[,] roomArray)
+    {
+        var officeRoomCount = 0;
+
+        while (officeRoomCount < MaxOfficeRooms)
+        {
+            var randomRow = Random.Range(0, numFloors);
+            var randomCol = Random.Range(0, roomsPerFloor);
+
+            if (!CanBeOffice(roomArray, randomRow, randomCol))
+            {
+                continue;
+            }
+
+            roomArray[randomRow, randomCol] = RoomTypeOffice;
+            officeRoomCount++;
+
+        }
+    }
+
+    private bool CanBeOffice(int[,] roomArray, int roomX, int roomY)
+    {
+        var roomId = roomArray[roomX, roomY];
+
+        if ((roomX == entranceCoordX) && (roomY == entranceCoordY))
+            return false;
+
+        if (roomId != RoomTypeEmpty)
+            return false;
+        
+        return true;
+    }
+    
     private void ConnectRooms()
     {
         for (int row = 0; row < gameMap.GetLength(0); row++)
