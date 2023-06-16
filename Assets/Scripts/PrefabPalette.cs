31d6f0dcbf394d080261c69360647b15d0bb5a15
Kazan33333 <kazan.kk@gmail.com>
2023-06-17 01:43:44 +0200

    Create PrefabPalette.cs

---
+++ Assets/Scripts/PrefabPalette.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class PrefabPalette : MonoBehaviour
+{
+    public GameObject[] prefabArray;
+
+    public GameObject GetRandomPrefab()
+    {
+        if (prefabArray.Length == 0)
+        {
+            Debug.LogError("The prefab array is empty!");
+            return null;
+        }
+
+        var randomPrefab = prefabArray[Random.Range(0, prefabArray.Length)];
+        return randomPrefab;
+    }
+}
